99c049a6f22307731678a92a10376319847ea0fc
Jannik Kevin Westenfelder <ubwsv@student.kit.edu>
2024-11-12 22:39:00 +0100

    adapt tbb hashmap to use gistStorage instead of segmented stack

--- src/customBnB/ST/combineGistAndDelayed/hashmap/TBBHashMapCombined.cpp
+++ src/customBnB/ST/combineGistAndDelayed/hashmap/TBBHashMapCombined.cpp
@@ -1,6 +1,4 @@
 #include "../../../structures/DelayedTasksList.hpp"
-#include "../../../structures/STEntry.hpp"
-#include "../../../structures/SegmentedStack.hpp"
 
 #include "IConcurrentHashMapCombined.h"
 
@@ -14,26 +12,29 @@
 // TODO the value should only be the delayed lsit not thw whole stEntry
 class TBBHashMapCombined : public IConcurrentHashMapCombined {
   using Key = int *;
-  using Value = STEntry *;
+  using Value = DelayedTasksList *;
 
   tbb::concurrent_hash_map<Key, Value, hashingCombined::VectorHasher> map_;
 
 public:
-  TBBHashMapCombined(std::vector<std::unique_ptr<SegmentedStack<>>> &Gist_storage)
+  TBBHashMapCombined(
+      std::vector<std::unique_ptr<GistStorage<>>> &Gist_storage)
       : IConcurrentHashMapCombined(Gist_storage){};
   DelayedTasksList *insert(Key key, bool value) override {
-    tbb::concurrent_hash_map<Key, Value, hashingCombined::VectorHasher>::accessor
-        accessor;
-    auto newEntry = createSTEntry(key, value);
-    const bool isNew = map_.insert(accessor, newEntry->gist.data());
+    tbb::concurrent_hash_map<Key, Value,
+                             hashingCombined::VectorHasher>::accessor accessor;
+    auto newEntry = createGistEntry(key);
+
+    const bool isNew = map_.insert(accessor, newEntry);
     if (isNew) {
-      accessor->second = newEntry;
+      accessor->second = value ? nullptr : reinterpret_cast<DelayedTasksList *>(-1);
       return value ? nullptr : reinterpret_cast<DelayedTasksList *>(-1);
     } else {
-      deleteSTEntry();
+      deleteGistEntry();
       if (value) {
-        if (accessor->second == nullptr) return nullptr; // TODO that should not be necessary
-        const auto delayed = std::move(accessor->second->taskList);
+        if (accessor->second == nullptr)
+          return nullptr; // TODO that should not be necessary
+        const auto delayed = std::move(accessor->second);
         accessor->second = nullptr;
         return delayed;
       } else {
@@ -43,23 +44,34 @@ public:
   }
 
   int find(const Key key) override {
-    tbb::concurrent_hash_map<Key, Value, hashingCombined::VectorHasher>::const_accessor
-        accessor;
+    tbb::concurrent_hash_map<
+        Key, Value, hashingCombined::VectorHasher>::const_accessor accessor;
     if (map_.find(accessor, key)) {
       // std::cout << accessor->first << " vs " << key << std::endl;
       return accessor->second == nullptr ? 2 : 1;
     }
     return 0;
   }
-  bool tryAddDelayed(std::shared_ptr<CustomTask> &task,
-                     int *key) override {
-    tbb::concurrent_hash_map<Key, Value, hashingCombined::VectorHasher>::accessor
-        accessor;
+  bool tryAddDelayed(std::shared_ptr<CustomTask> &task, int *key) override {
+    // std::cout << "try pause" << std::endl;
+    tbb::concurrent_hash_map<Key, Value,
+                             hashingCombined::VectorHasher>::accessor accessor;
     if (map_.find(accessor, key)) {
-      if (accessor->second->taskList == nullptr)
+      auto st = accessor->second;
+      auto list = accessor->second;
+      // check for nullptr
+      if (!accessor->second)
         return false;
-      accessor->second->taskList =
-          new DelayedTasksList(task, accessor->second->taskList);
+      //   std::stringstream gis;
+      // gis << accessor->second->taskList << " ";
+      accessor->second =
+          new DelayedTasksList(task, accessor->second);
+      // gis << "paused" << " ";
+      // for (auto vla : task->state)
+      //   gis << vla << ", ";
+      // gis << " Job: " << task->job << accessor->second->taskList->next <<  "\n";
+      // std::cout << gis.str() << std::endl;
+      // std::cout << "delayed" << std::endl;
       return true;
     } else
       return false;
