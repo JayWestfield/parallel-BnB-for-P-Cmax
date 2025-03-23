774aa393faa9b24519e9a0c8f3fb022fc12ea2c5
Jannik Kevin Westenfelder <ubwsv@student.kit.edu>
2025-03-23 15:32:42 +0100

    180 s timeout

--- benchmarkExecuter/experimentRefactorGrowtAnni.sh
+++ benchmarkExecuter/experimentRefactorGrowtAnni.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./build/full_refactored 64 60 200 benchmarks anni benchmarks/sampledBenchmarks/anni-100.txt
+./build/full_refactored 64 180 200 benchmarks anni benchmarks/sampledBenchmarks/anni-100.txt
