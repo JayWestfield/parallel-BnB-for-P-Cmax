d52e3299ee24d3e77aa50a6331bcba59b5091f98
Jannik Kevin Westenfelder <ubwsv@student.kit.edu>
2025-03-25 18:08:06 +0100

    added combined benchmark

---
+++ benchmarkExecuter/combined.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+# Check if arguments are provided, otherwise use default values
+THREADS=${1:-64}   # Default: 64
+TIMEOUT=${2:-30}  # Default: 180
+CONFIG=${3:-200}   # Default: 200
+
+
+
+# List of benchmark names
+BENCHMARKS="berndt cnf frangioni graph huebner laupichler lawrinenko lehmann planted schreiber anni"
+
+# Loop through each benchmark and execute the command
+for BENCHMARK in $BENCHMARKS; do
+  ./build/full_refactored $THREADS $TIMEOUT $CONFIG benchmarks $BENCHMARK benchmarks/sampledBenchmarks/${BENCHMARK}-25.txt
+done
