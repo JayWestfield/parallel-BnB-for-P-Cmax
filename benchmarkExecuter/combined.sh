254a63d5868649ddb008da424613ad0192443ea9
Jannik Kevin Westenfelder <ubwsv@student.kit.edu>
2025-04-10 10:40:32 +0200

    update benchmark bashes to choose benchmark based of parameter

--- benchmarkExecuter/combined.sh
+++ benchmarkExecuter/combined.sh
@@ -2,9 +2,10 @@
 
 
 # Check if arguments are provided, otherwise use default values
-THREADS=${1:-64}   # Default: 64
-TIMEOUT=${2:-30}  # Default: 180
-CONFIG=${3:-200}   # Default: 200
+THREADS=${1:-64}
+TIMEOUT=${2:-30}
+CONFIG=${3:-200}
+BENCHMARK_TYPE=${2:-50}
 
 
 
@@ -13,5 +14,5 @@ BENCHMARKS="berndt cnf frangioni graph huebner laupichler lawrinenko lehmann pla
 
 # Loop through each benchmark and execute the command
 for BENCHMARK in $BENCHMARKS; do
-  ./build/full_refactored $THREADS $TIMEOUT $CONFIG benchmarks $BENCHMARK benchmarks/sampledBenchmarks/${BENCHMARK}-combined.txt
+  ./build/full_refactored $THREADS $TIMEOUT $CONFIG benchmarks $BENCHMARK benchmarks/sampledBenchmarks/${BENCHMARK}-${BENCHMARK_TYPE}.txt
 done
--- benchmarkExecuter/reference.sh
+++ benchmarkExecuter/reference.sh
@@ -2,10 +2,11 @@
 
 
 # Check if arguments are provided, otherwise use default values
-TIMEOUT=${1:-30}  # Default: 180
+TIMEOUT=${1:-30}
+BENCHMARK_TYPE=${2:-50}
 
 
-ulimit -s 65536  # Set stack size to 32 MB
+ulimit -s 65536  # increase stackSize
 
 # List of benchmark names
 BENCHMARKS="berndt cnf frangioni graph huebner laupichler lawrinenko lehmann planted schreiber anni"
@@ -13,5 +14,5 @@ BENCHMARKS="berndt cnf frangioni graph huebner laupichler lawrinenko lehmann pla
 # Loop through each benchmark and execute the command
 for BENCHMARK in $BENCHMARKS; do
   echo 'Start Experiment with timeout:' $TIMEOUT 'for benchmark: ' $BENCHMARK 
-  python3 ./src/experiments/ref_run_benchmarks.py $TIMEOUT ./benchmarks $BENCHMARK ./benchmarks/sampledBenchmarks/${BENCHMARK}-50.txt
+  python3 ./src/experiments/ref_run_benchmarks.py $TIMEOUT ./benchmarks $BENCHMARK ./benchmarks/sampledBenchmarks/${BENCHMARK}-${BENCHMARK_TYPE}.txt
 done
