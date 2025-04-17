4bc4e2656f128e35b9011fa1068387403989908d
Jannik Kevin Westenfelder <ubwsv@student.kit.edu>
2025-04-17 13:22:06 +0200

    Add more Configs

--- src/_refactoredBnB/SolverConfig.hpp
+++ src/_refactoredBnB/SolverConfig.hpp
@@ -30,6 +30,51 @@ SolverConfig getSolverConfig(int version) {
     return {static_cast<int>(pow(2, 21)), 100, 1, templateOptimization, 0};
   case 6:
     return {static_cast<int>(pow(2, 24)), 100, 1, templateOptimization, 0};
+  case 7:
+    return {static_cast<int>(pow(2, 27)), 100, 1, templateOptimization, 0};
+  case 8:
+    return {static_cast<int>(pow(2, 30)), 100, 1, templateOptimization, 0};
+  case 9:
+    return {static_cast<int>(pow(2, 33)), 100, 1, templateOptimization, 0};
+  case 10:
+    return {static_cast<int>(pow(2, 16)), static_cast<int>(pow(2, 4)),
+            static_cast<int>(pow(2, 0)), templateOptimization, 0};
+  case 11:
+    return {static_cast<int>(pow(2, 16)), static_cast<int>(pow(2, 6)),
+            static_cast<int>(pow(2, 0)), templateOptimization, 0};
+  case 12:
+    return {static_cast<int>(pow(2, 16)), static_cast<int>(pow(2, 8)),
+            static_cast<int>(pow(2, 0)), templateOptimization, 0};
+  case 13:
+    return {static_cast<int>(pow(2, 16)), static_cast<int>(pow(2, 10)),
+            static_cast<int>(pow(2, 0)), templateOptimization, 0};
+  case 14:
+    return {static_cast<int>(pow(2, 16)), static_cast<int>(pow(2, 10)),
+            static_cast<int>(pow(2, 0)), templateOptimization, 0};
+  case 15:
+    return {static_cast<int>(pow(2, 16)), static_cast<int>(pow(2, 12)),
+            static_cast<int>(pow(2, 0)), templateOptimization, 0};
+  case 16:
+    return {static_cast<int>(pow(2, 16)), static_cast<int>(pow(2, 14)), 1,
+            templateOptimization, 0};
+  case 17:
+    return {static_cast<int>(pow(2, 16)), static_cast<int>(pow(2, 10)),
+            static_cast<int>(pow(2, 0)), templateOptimization, 0};
+  case 18:
+    return {static_cast<int>(pow(2, 16)), static_cast<int>(pow(2, 10)),
+            static_cast<int>(pow(2, 1)), templateOptimization, 0};
+  case 19:
+    return {static_cast<int>(pow(2, 16)), static_cast<int>(pow(2, 10)),
+            static_cast<int>(pow(2, 2)), templateOptimization, 0};
+  case 20:
+    return {static_cast<int>(pow(2, 16)), static_cast<int>(pow(2, 10)),
+            static_cast<int>(pow(2, 3)), templateOptimization, 0};
+  case 21:
+    return {static_cast<int>(pow(2, 16)), static_cast<int>(pow(2, 10)),
+            static_cast<int>(pow(2, 4)), templateOptimization, 0};
+  case 22:
+    return {static_cast<int>(pow(2, 16)), static_cast<int>(pow(2, 10)),
+            static_cast<int>(pow(2, 5)), templateOptimization, 0};
   default:
     throw std::invalid_argument("Invalid STVersion: " +
                                 std::to_string(version));
